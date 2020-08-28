6f8b828c74ee97f10c0a8774f926be32b83473e8
Ivanov Fedor <ivnfedor@gmail.com>
2020-08-28 20:02:21 +0300

    Fixed notion-py import

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import sys
 
-from notion.smoke_test import run_live_smoke_test
+from third_party.notion.smoke_test import run_live_smoke_test
 
 if __name__ == "__main__":
     description = "Run notion-py client smoke tests"
